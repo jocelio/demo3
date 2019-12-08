3a7b19db88ed3483116ac18b660824b7d6dec72d
Jocélio Lima <jocelio.lima@joyjet.com>
2019-12-08 10:18:45 -0300

    adding cors config

---
+++ src/main/java/com/example/demo3/security/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo3.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("*")
+                .allowedOrigins("https://shrouded-gorge-13128.herokuapp.com","http://localhost:3000")
+                .allowedMethods("PUT", "DELETE","GET","POST")
+                .allowCredentials(false).maxAge(3600);
+    }
+}
