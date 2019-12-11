101428eb016ae95295325fae3a4abb26e4efa077
Jocélio Lima <jocelio.lima@joyjet.com>
2019-12-11 14:56:16 -0300

    Adding rule rest

---
+++ src/main/java/com/example/demo3/rest/federation/RuleController.java
@@ -0,0 +1,30 @@
+package com.example.demo3.rest.federation;
+
+
+import com.example.demo3.repository.RuleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import static org.springframework.http.HttpStatus.*;
+
+
+@RestController()
+@RequestMapping("api/v1/rules")
+public class RuleController {
+
+    @Autowired
+    RuleRepository ruleRepository;
+
+    @GetMapping()
+    public ResponseEntity getAll() {
+        return ResponseEntity.status(OK).body(ruleRepository.findAll());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity get(@PathVariable(value = "id") Long id) {
+        return ResponseEntity.status(CREATED).body(ruleRepository.getOne(id));
+    }
+
+}
